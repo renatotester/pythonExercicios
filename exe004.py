d7dbbd4b021d125de5310f64c03577d4555895f3
Renato Bernardo <105286206+renatotester@users.noreply.github.com>
2025-07-01 12:10:15 -0300

    Update exe004.py

--- exe004.py
+++ exe004.py
@@ -1,12 +1,21 @@
 #faca um progrma que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todos as informações possíveis sobre ele.
 print('Olá Mundo!')
-a=input('Digite algo: ')
-#O ""a" é o que nós chamamos de objeto.
-print('O tipo primitivo desse valor é',type(a))
-print('Só tem espaços?',a.isspace())
-print('É um número?',a.isnumeric())
-print('É alfabético?',a.isalpha())
-print('É alfanumérico?',a.isalnum())
-print('Está em maiúsculo?',a.isupper())
-print('Está em minúscula?',a.islower())
-print('Está capitalizada?',a.istitle())
+def analisar_entrada():
+    """
+    Lê uma entrada do usuário e exibe informações detalhadas sobre ela.
+    """
+    valor = input('Digite algo: ')
+    print('-' * 40)
+    print(f'O tipo primitivo desse valor é {type(valor)}')
+    print(f'Só tem espaços? {valor.isspace()}')
+    print(f'É numérico? {valor.isnumeric()}')
+    print(f'É alfabético? {valor.isalpha()}')
+    print(f'É alfanumérico? {valor.isalnum()}')
+    print(f'Está em maiúsculas? {valor.isupper()}')
+    print(f'Está em minúsculas? {valor.islower()}')
+    print(f'Está capitalizada? {valor.istitle()}')
+    print('-' * 40)
+
+if __name__ == "__main__":
+    print('Olá, Mundo!')
+    analisar_entrada()
